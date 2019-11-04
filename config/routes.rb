9206a7c3816339292a1c98105f5281ff35b4c8d2
ashna shah <a5hna@hotmail.co.uk>
2019-11-04 17:18:48 +0800

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,16 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :boards, except: [:index] do
+    resources :board_users, only: [:index, :new, :create]
+    resources :experiences, only: [:new, :create]
+    resources :day_schedules, only: [:show, :new, :create]
+  end
+
+  resources :board_users, only: [:destroy]
+  resources :experiences, only: [:edit, :update, :destroy]
+  resources :day_schedules, only: [:destroy]
+  resources :friends, only: [:index, :new, :create, :destroy]
+
+  get 'dashboard', to: "pages#dashboard"
 end
