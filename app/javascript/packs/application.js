66c8e0c26f4a5ba05e6e729f7df94275f20a3304
karim Tidjani <karimtidjanilopez@icloud.com>
2019-11-07 10:08:47 +0100

    new navbar and change login and signup form

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,13 @@
 import "bootstrap";
+
+
+
+
+const input = document.querySelector("#friends-search");
+input.addEventListener("keyup", (event) => {
+  console.log(event.currentTarget.value)// what the current user typed
+});
+
+
+// make allt the users display none
+// inside the event listener, change to display block if user name matches what the current user typed
