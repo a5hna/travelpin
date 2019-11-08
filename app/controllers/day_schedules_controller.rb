285dbac0fffbec5947f1700ddc6db888dc14afe4
karim Tidjani <karimtidjanilopez@icloud.com>
2019-11-08 10:21:14 +0100

    new new day schedule index

--- app/controllers/day_schedules_controller.rb
+++ app/controllers/day_schedules_controller.rb
@@ -1,6 +1,7 @@
 class DaySchedulesController < ApplicationController
   def index
-    @day_schedules = Board.find(params[:board_id]).day_schedules
+    @board = Board.find(params[:board_id])
+    @day_schedules = @board.day_schedules
 
   end
 
