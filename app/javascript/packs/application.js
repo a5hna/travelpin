b7d1351c795d27b72b0bfe8e05bc701cb2a16744
ashna shah <a5hna@hotmail.co.uk>
2019-11-13 18:29:12 +0800

    added notification count when adding to day schedule. also changed the styling

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,21 +4,32 @@ import "tooltip.js"
 
 const experienceCard = document.querySelectorAll(".experience-card");
 
+const notificationCount = document.getElementById("notification");
+
 const calendar = document.querySelector("#experiences");
 const toggleActiveClass = (event) => {
   const boardShowPage = document.getElementById('board-show');
 
   if (boardShowPage) {
-    if (event.currentTarget.classList.contains('active')) {
+    if (event.currentTarget.classList.contains('active')) {console.log(notificationCount)
       event.currentTarget.classList.remove('active');
-    } else {
+      notificationCount.innerText = Number.parseInt(notificationCount.innerText, 10) - 1
+      if (Number.parseInt(notificationCount.innerText, 10) === 0 ) {
+        notificationCount.style.display = "none"
+      }
+    } else  {
+        notificationCount.style.display = "flex"
+
+
       event.currentTarget.classList.add('active');
       const id = event.currentTarget.dataset.id;
       const initialLink = calendar.getAttribute('href');
       calendar.value = calendar.value + `${id}&`
+      notificationCount.innerText = Number.parseInt(notificationCount.innerText, 10) + 1
+      }
     }
   }
-};
+;
 
 const toggleActiveOnClick = (element) => {
   element.addEventListener("click", toggleActiveClass);
