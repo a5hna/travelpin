7910a400a0656d3a47990225d19e02d22be3867d
Santiago Campos <ycl@outlook.com>
2019-11-13 12:51:25 +0800

    pwa sorta working

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,8 +1,12 @@
 Rails.application.configure do
   config.action_mailer.default_url_options = { host: "http://localhost:3000" }
+
+  #ngrok blockage solver
+  config.hosts << '.ngrok.io'
+
   # Settings specified here will take precedence over those in config/application.rb.
 
-  # In the development environment your application's code is reloaded on
+  # In the development environment your application's codeis reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,9 +1,12 @@
 Rails.application.configure do
   config.action_mailer.default_url_options = { host: "http://TODO_PUT_YOUR_DOMAIN_HERE" }
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
   config.cache_classes = true
+
+
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
